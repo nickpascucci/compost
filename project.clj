e46709eb0dd88b9cfed1b70a6675440426c7652f
Nick Pascucci <npascut1@gmail.com>
2015-05-20 00:10:14 -0400

    Remove cljs dependencies.

--- project.clj
+++ project.clj
@@ -11,7 +11,6 @@
                  [javax.servlet/servlet-api "2.5"]
                  [liberator "0.10.0"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
                  [prismatic/dommy "0.1.2"]
                  [prismatic/schema "0.2.1"]
@@ -20,27 +19,11 @@
                  [ring/ring-devel "1.1.0"]
                  [ring/ring-json "0.2.0"]
                  [speclj "2.9.1"]
-                 [specljs "2.9.1"]
                  ]
   :min-lein-version "2.0.0"
   :main compost.core
   :aot [compost.core]
   :uberjar-name "compost-standalone.jar"
-  :plugins [[lein-cljsbuild "1.0.1"]
-            [environ/environ.lein "0.2.1"]
-            ]
-  :hooks [environ.leiningen.hooks
-          leiningen.cljsbuild]
-  :profiles {:production {:env {:production true}}}
-  :cljsbuild {
-              :builds {:release {:source-paths ["src-cljs"]
-                                 :compiler {:output-to "resources/public/js/main.js"
-                                            :optimizations :whitespace
-                                            :pretty-print true}
-                                 :jar true}
-                       :test {:source-paths ["src-cljs" "test-cljs"]
-                              :compiler {:output-to "target/unit-test.js"
-                                         :optimizations :whitespace
-                                         :pretty-print true}
-                              :notify-command ["resources/specljs-runner.js"
-                                               "target/unit-test.js"]}}})
+  :plugins [[environ/environ.lein "0.2.1"]]
+  :hooks [environ.leiningen.hooks]
+  :profiles {:production {:env {:production true}}})
