0f0a135680c488640f8ea1f36f858dc4c5b61f02
Nick Pascucci <npascut1@gmail.com>
2015-11-07 12:30:51 -0500

    Refactor EditorService.

--- resources/public/js/compost-min.js
+++ resources/public/js/compost-min.js
@@ -262,71 +262,78 @@ angular.module("editorModule", ["ngMaterial"])
     .service("EditorService", EditorService);
 
 function EditorService($rootScope, $q, $state, $mdDialog, authService) {
-    var service = {
-        useDialog: !isMobile(),
-        deferred: undefined,
-        create: function() {
-            var now = moment().startOf("day");
-            var food = {
-                "name": "New Food",
-                "created": momentToIsoString(now),
-                "expires": momentToIsoString(now),
-                "owner": authService.getUserEmail(),
-                "quantity": 1,
-                "status": "active",
-                "frozen?": false,
-            };
-            return service.edit(food);
-        },
-        edit: function(food) {
-            if (service.useDialog) {
-                return service.editWithDialog(food);
-            } else {
-                return service.editWithPage(food);
-            }
-        },
-        editWithDialog: function(food) {
-            return $mdDialog.show({
-                clickOutsideToClose: true,
-                templateUrl: "partials/editor-dialog.html",
-                controller: "EditorController",
-                controllerAs: "editorCtrl",
-                parent: angular.element(document.body),
-                locals: {
-                    food: food
-                },
-                bindToController: true
-            });
-        },
-        editWithPage: function(food) {
-            service.deferred = $q.defer();
-            console.log('Transferring to editor page.');
-            $state.go('app.foods.edit', {foodId: food.id,
-                                         food: JSON.stringify(food)});
-            return service.deferred.promise;
-        },
-        cancelEdit: function() {
-            console.log("Canceled editing");
-            if (service.useDialog) {
-                $mdDialog.cancel();
-            } else {
-                service.deferred.reject("Canceled");
-                $state.go('app.foods');
-            }
-        },
-        finishEdit: function(food) {
-            if (service.useDialog) {
-                $mdDialog.hide(food);
-            } else {
-                service.deferred.resolve(food);
-                console.log("Done editing:", food);
-                $state.go('app.foods');
-            }
-        }
-    };
+    this.q_ = $q;
+    this.state_ = $state;
+    this.mdDialog_ = $mdDialog;
+    this.authService_ = authService;
 
-    return service;
+    this.useDialog = !isMobile();
+    this.deferred = undefined;
 }
+
+EditorService.prototype.create = function() {
+    var now = moment().startOf("day");
+    var food = {
+        "name": "New Food",
+        "created": momentToIsoString(now),
+        "expires": momentToIsoString(now),
+        "owner": this.authService_.getUserEmail(),
+        "quantity": 1,
+        "status": "active",
+        "frozen?": false,
+    };
+    return this.edit(food);
+};
+
+EditorService.prototype.edit = function(food) {
+    if (this.useDialog) {
+        return this.editWithDialog(food);
+    } else {
+        return this.editWithPage(food);
+    }
+};
+
+EditorService.prototype.editWithDialog = function(food) {
+    return this.mdDialog_.show({
+        clickOutsideToClose: true,
+        templateUrl: "partials/editor-dialog.html",
+        controller: "EditorController",
+        controllerAs: "editorCtrl",
+        parent: angular.element(document.body),
+        locals: {
+            food: food
+        },
+        bindToController: true
+    });
+};
+
+EditorService.prototype.editWithPage = function(food) {
+    this.deferred = this.q_.defer();
+    console.log('Transferring to editor page.');
+    this.state_.go('app.foods.edit', {foodId: food.id,
+                                 food: JSON.stringify(food)});
+    return this.deferred.promise;
+};
+
+EditorService.prototype.cancelEdit = function() {
+    console.log("Canceled editing");
+    if (this.useDialog) {
+        this.mdDialog_.cancel();
+    } else {
+        this.deferred.reject("Canceled");
+        this.state_.go('app.foods');
+    }
+};
+
+EditorService.prototype.finishEdit = function(food) {
+    if (this.useDialog) {
+        this.mdDialog_.hide(food);
+    } else {
+        this.deferred.resolve(food);
+        console.log("Done editing:", food);
+        this.state_.go('app.foods');
+    }
+};
 
 var foodListModule = angular.module("foodListModule", [
     "ngMaterial",
